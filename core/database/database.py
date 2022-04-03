d326708d87cf9129f56cb3ddcad3285763909b91
Barry Wang <wyc6794@gmail.com>
2022-04-03 21:05:35 +0800

    feat(config): support config for different environment

--- core/database/database.py
+++ core/database/database.py
@@ -2,14 +2,13 @@ from urllib.parse import quote
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import json
 
+with open("./config.json") as config_file:
+    config = json.load(config_file)['sql_config']
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://libadmin:%s@localhost:3306/libsense' % quote('rocky!mysql')
+SQLALCHEMY_DATABASE_URL = f"""mysql+pymysql://{config["username"]}:%s@{config["server_addr"]}:{config["server_port"]}/libsense""" % quote(
+    config['password'])
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-
-
-
