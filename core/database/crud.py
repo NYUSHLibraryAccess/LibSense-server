95d8cb9cac5cafef4c25bc444a05fda8b8253716
Barry Wang <wyc6794@gmail.com>
2022-04-26 02:03:02 +0800

    fix: minor code fix

--- core/database/crud.py
+++ core/database/crud.py
@@ -27,7 +27,7 @@ def get_all_orders(db: Session, start_idx: int = 0, limit: int = 10, filters=Non
     total_records = query.count()
     if limit:
         query = query.limit(limit)
-    return query.all(), total_records
+    return query.all(), start_idx * limit + total_records
 
 
 def get_order_detail(db: Session, order_id: int):
