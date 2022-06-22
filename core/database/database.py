21cd0f2cb0b5b934376e89736342837e783a4f19
Barry Wang <wyc6794@gmail.com>
2022-06-22 19:49:13 +0800

    fix(db): add db connection daemon

--- core/database/database.py
+++ core/database/database.py
@@ -9,6 +9,6 @@ with open("configs/config.json") as config_file:
 
 SQLALCHEMY_DATABASE_URL = f"""mysql+pymysql://{config["username"]}:%s@{config["server_addr"]}:{config["server_port"]}/{config["database"]}""" % quote(
     config['password'])
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
